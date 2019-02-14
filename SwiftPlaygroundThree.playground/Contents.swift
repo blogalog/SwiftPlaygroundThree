20e214c8affce4e36080abfd28d3eaf6906c80c6
Brandon <blogalog@hawaii.edu>
2019-02-14 10:29:27 -1000

    Completed in class Week 6

--- SwiftPlaygroundThree.playground/Contents.swift
+++ SwiftPlaygroundThree.playground/Contents.swift
@@ -4,3 +4,20 @@ import UIKit
 
 var str = "Hello, playground"
 
+//declaration
+var fruitNames: [String]
+
+// making an assignment
+fruitNames = ["Watermelon","Mango","Peach"]
+print(fruitNames)
+
+//string interpolation example *not an iteration
+print("I like to eat \(fruitNames)")
+
+//iterate using for-in syntax
+for name in fruitNames {
+    print("I like to eat " + name)
+}
+
+print(fruitNames[1])
+print(fruitNames[0])
